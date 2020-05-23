c0aeee9ded2b7766edc776357f42e257fa4d7e20
jojojojo1322 <bobob935@gmail.com>
2020-05-23 19:31:39 +0900

    Home fix

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function About() {
-  return <span>About this page: I built it because I love movies.</span>;
+  return <h1>하이</h1>;
 }
 
 export default About;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,32 +6,33 @@ import "./Home.css";
 class Home extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
   };
+
   getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
+    const movies = await (
+      await axios.get(
+        "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+      )
+    ).data.data.movies;
     this.setState({ movies, isLoading: false });
   };
+
   componentDidMount() {
     this.getMovies();
   }
+
   render() {
-    const { isLoading, movies } = this.state;
+    const { movies, Load } = this.state;
     return (
       <section className="container">
-        {isLoading ? (
+        {Load ? (
           <div className="loader">
             <span className="loader__text">Loading...</span>
           </div>
         ) : (
           <div className="movies">
-            {movies.map(movie => (
+            {movies.map((movie) => (
               <Movie
                 key={movie.id}
                 id={movie.id}
